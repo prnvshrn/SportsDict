920672d454d20669e4b5dd55bd8beb3c55912af7
prnvshrn <prnvshrn@gmail.com>
2018-07-23 00:39:04 -0700

    Added props to apollo client

--- client/src/components/main_components.js
+++ client/src/components/main_components.js
@@ -3,10 +3,82 @@ import ReactDOM from 'react-dom';
 import {FormGroup, FormControl, Button} from 'react-bootstrap';
 import ApolloClient from "apollo-boost";
 import gql from "graphql-tag";
+import {Query, ApolloProvider} from 'react-apollo';
+
+const name = 'Gary Cahill';
 
 const client = new ApolloClient({
   		uri: "graphql"
 	});
+
+const football_query = gql`
+	      query football($breed: String!){
+		  football(name:$breed) {
+		    name,
+		    position,
+		    goals,
+		    chances_created,
+		    defensive_actions
+		  }
+		}`;
+
+const Pest = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My first Apollo app</h2>
+		<ExchangeRates/>
+    </div>
+  </ApolloProvider>
+);
+
+class Numpy extends Component{
+constructor(props, context) {
+    super(props, context);
+}
+	render(){
+		//alert(this.props.name);
+		return(
+			<div>
+			<ApolloProvider client={client}>
+		    <div>
+		      <h2>My first Apollo app</h2>
+			  <Query
+    query={football_query} variables={{breed: this.props.name}}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return data.football.map(({ name, position }) => (
+        <div key={name}>
+          <p>{name}{position}</p>
+        </div>
+      ));
+    }}
+  </Query>	
+		    </div>
+		  	</ApolloProvider>
+			</div>
+		);
+	}
+}
+
+const ExchangeRates = () => (
+  <Query
+    query={football_query} variables={{breed: 'Gary Cahill'}}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return data.football.map(({ name, position }) => (
+        <div key={name}>
+          <p>{name}{position}</p>
+        </div>
+      ));
+    }}
+  </Query>
+);
 
 class MainPanel extends Component{
 	constructor(props, context) {
@@ -26,22 +98,11 @@ class MainPanel extends Component{
   handlesubmit(event){
   	event.preventDefault();
   	console.log(this.state.value);
-
-	client
+  	client
 	  .query({
-	    query: gql`
-	      {
-		  football(name:"Gary Cahill") {
-		    name,
-		    position,
-		    goals,
-		    chances_created,
-		    defensive_actions
-		  }
-		}
-	    `
+	    query: football_query,
 	  })
-	  .then(result => console.log(result));  	
+	  .then(console.log);
   }
 
 	render(){
@@ -58,6 +119,8 @@ class MainPanel extends Component{
 			</FormGroup>
 			<Button type="submit" bsStyle="success">Go</Button>
 			</form>
+			<Pest/>
+			<Numpy name={this.state.value}/>
 			</div>
 		);
 	}
